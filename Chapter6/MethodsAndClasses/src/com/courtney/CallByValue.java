d9a37919ff9e3c5ac95fa4e6b58b29210e526f0d
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-05 11:23:19 +0200

    Added Files

--- Chapter6/MethodsAndClasses/src/com/courtney/CallByValue.java
+++ Chapter6/MethodsAndClasses/src/com/courtney/CallByValue.java
@@ -2,16 +2,17 @@ package com.courtney;
 
 class CallByValue {
     public static void main(String args[]) {
-        Test ob = new Test();
+//        Test ob = new Test();
+//        int a = 15, b = 20;
+//
+//        System.out.println("a and b before call: " +
+//                a + " " + b);
+//
+//        ob.noChange(a, b);
+//
+//        System.out.println("a and b after call: " +
+//                a + " " + b);
+//    }
 
-        int a = 15, b = 20;
-
-        System.out.println("a and b before call: " +
-                a + " " + b);
-
-        ob.noChange(a, b);
-
-        System.out.println("a and b after call: " +
-                a + " " + b);
     }
 }
--- Chapter6/MethodsAndClasses/src/com/courtney/Test.java
+++ Chapter6/MethodsAndClasses/src/com/courtney/Test.java
@@ -1,11 +1,17 @@
 package com.courtney;
 
-// Simple types are passed by value.
+// Objects are passed through their references.
 class Test {
-    /* This method causes no change to the arguments
-       used in the call. */
-    void noChange(int i, int j) {
-        i = i + j;
-        j = -j;
+    int a, b;
+
+    Test(int i, int j) {
+        a = i;
+        b = j;
+    }
+    /* Pass an object. Now, ob.a and ob.b in object
+       used in the call will be changed. */
+    void change(Test ob) {
+        ob.a = ob.a + ob.b;
+        ob.b = -ob.b;
     }
 }
