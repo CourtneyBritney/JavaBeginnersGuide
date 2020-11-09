ffbf3f15bbbb5477fd43b518cfb3b46c15056924
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-09 10:37:38 +0200

    Added Files

---
+++ Chapter7/Inheritance/src/com/courtney/IncompatibleRef.java
@@ -0,0 +1,13 @@
+package com.courtney;
+
+class IncompatibleRef {
+    public static void main(String args[]) {
+        X x = new X(10);
+        X x2;
+        Y y = new Y(5);
+
+        x2 = x; // OK, both of same type
+
+        x2 = x2; // Error, not of same type
+    }
+}
---
+++ Chapter7/Inheritance/src/com/courtney/X.java
@@ -0,0 +1,14 @@
+package com.courtney;
+
+// This will not compile.
+class X {
+    int a;
+
+    X(int i) { a = i; }
+}
+
+class Y {
+    int a;
+
+    Y(int i) { a = i; }
+}
