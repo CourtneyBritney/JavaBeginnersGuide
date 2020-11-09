0177f899fc112d941aae30f97680e192afd6baa7
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-09 10:56:19 +0200

    Added Files

---
+++ Chapter7/Inheritance/src/com/courtney/Sup.java
@@ -0,0 +1,21 @@
+package com.courtney;
+
+// Demonstrate dynamic method dispatch.
+
+class Sup {
+    void who() {
+        System.out.println("who() in Sup");
+    }
+}
+
+class Sub1 extends Sup {
+    void who() {
+        System.out.println("who() in Sub1");
+    }
+}
+
+class Sub2 extends Sup {
+    void who() {
+        System.out.println("who() in Sub2");
+    }
+}
