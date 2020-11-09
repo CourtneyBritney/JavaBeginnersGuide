5658ba0604257faba835103e1f17c3a038b4989a
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-09 10:39:45 +0200

    Added Files

--- Chapter7/Inheritance/src/com/courtney/IncompatibleRef.java
+++ Chapter7/Inheritance/src/com/courtney/IncompatibleRef.java
@@ -4,7 +4,7 @@ class IncompatibleRef {
     public static void main(String args[]) {
         X x = new X(10);
         X x2;
-        Y y = new Y(5);
+       // Y y = new Y(5);
 
         x2 = x; // OK, both of same type
 
--- Chapter7/Inheritance/src/com/courtney/X.java
+++ Chapter7/Inheritance/src/com/courtney/X.java
@@ -1,14 +1,17 @@
 package com.courtney;
 
-// This will not compile.
+// A superclass reference can refer to a subclass object.
 class X {
     int a;
 
     X(int i) { a = i; }
 }
 
-class Y {
-    int a;
+class Y extends X {
+    int b;
 
-    Y(int i) { a = i; }
+    Y(int i, int j) {
+        super(j);
+        b = i;
+    }
 }
