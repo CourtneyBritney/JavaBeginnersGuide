e3d607733e106416d80c2c9e21d8080c8d70b559
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-10 10:43:18 +0200

    Added Files

---
+++ Chapter9/ExceptionHandling/src/com/courtney/NonIntResultException.java
@@ -0,0 +1,19 @@
+package com.courtney;
+
+// Use a custom exception.
+
+// Create an exception.
+class NonIntResultException extends Exception {
+    int n;
+    int d;
+
+    NonIntResultException(int i, int j) {
+        n = i;
+        d = j;
+    }
+
+    public String toString() {
+        return "Result of " + n + " / " + d +
+                " is non-integer.";
+    }
+}
