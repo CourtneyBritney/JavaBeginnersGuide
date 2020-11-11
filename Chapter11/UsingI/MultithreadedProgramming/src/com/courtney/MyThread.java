3c80f081aedc85907cdd701a2388045e750e7787
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-11 14:25:47 +0200

    Added Files

--- Chapter11/UsingI/MultithreadedProgramming/src/com/courtney/MyThread.java
+++ Chapter11/UsingI/MultithreadedProgramming/src/com/courtney/MyThread.java
@@ -1,27 +1,41 @@
 package com.courtney;
 
-// Create a thread by implementing Runnable.
+// MyThread variations. This version of MyThread
+// creates a Thread when its constructor is called and
+// stores it in an instance variable called thrd.
+// It also sets the name of the thread and provides
+// a factory method to create and start a thread.
 
 class MyThread implements Runnable {
-    String thrdName;
+    Thread thrd;
 
+    // Construct a new thread using this Runnable and give
+    // it a name.
     MyThread(String name) {
-        thrdName = name;
+        thrd = new Thread(this, name);
+    }
+
+    // A factory method that creates and starts a thread.
+    public static MyThread createAndStart(String name) {
+        MyThread myThrd = new MyThread(name);
+
+        myThrd.thrd.start(); // start the thread
+        return myThrd;
     }
 
     // Entry point of thread.
     public void run() {
-        System.out.println(thrdName + " starting.");
+        System.out.println(thrd.getName() + " starting.");
         try {
-            for(int count=0; count < 10; count++) {
+            for(int count=0; count<10; count++) {
                 Thread.sleep(400);
-                System.out.println("In " + thrdName +
+                System.out.println("In " + thrd.getName() +
                         ", count is " + count);
             }
         }
         catch(InterruptedException exc) {
-            System.out.println(thrdName + " interrupted.");
+            System.out.println(thrd.getName() + " interrupted.");
         }
-        System.out.println(thrdName + " terminating.");
+        System.out.println(thrd.getName() + " terminating.");
     }
 }
