649a8b872eaa60fcbf4de24fb59a280cacc5b505
CourtneyBritney <courtney02alexander@gmail.com>
2020-11-11 15:39:45 +0200

    Added Files

--- Chapter12/EnumAutoboxing/src/com/courtney/Transport.java
+++ Chapter12/EnumAutoboxing/src/com/courtney/Transport.java
@@ -1,8 +1,15 @@
 package com.courtney;
 
-// An enumeration of Transport varieties.
+// Use an enum constructor, instance variable, and method.
 enum Transport {
-    CAR, TRUCK, AIRPLANE, TRAIN, BOAT
+    CAR(65), TRUCK(55), AIRPLANE(600), TRAIN(70), BOAT(22);
+
+    private int speed; // typical speed of each transport
+
+    // Constructor
+    Transport(int s) { speed = s; }
+
+    int getSpeed() { return speed; }
 }
 
 class EnumDemo {
